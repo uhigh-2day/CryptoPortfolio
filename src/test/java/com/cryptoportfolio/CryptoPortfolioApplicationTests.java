17616e325795e912531e03e3e197d0b0a5be3ef8
uhigh-2day <uhigh8840@gmail.com>
2025-05-26 11:52:12 +0530

    [Sahil] -Added:Project Structure

---
+++ src/test/java/com/cryptoportfolio/CryptoPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptoportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
